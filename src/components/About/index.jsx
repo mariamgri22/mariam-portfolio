94038c7e40fb18e428755d48fb802b495dc86bd5
mariamgri22 <mariamgrigoryan99@gmail.com>
2022-05-10 22:53:06 +0400

    fix:bug

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,10 +6,10 @@ export default function About() {
   return (
     <>
     <div className='about'>
-      <img className='image' src={imageMar}/>
+      <img className='image' src={imageMar} alt="img Mar"/>
       <div>
         <p>My Info</p>
-        My first laptop was made of a box of candies, the keyboard և screen of which was painted with markers, and the screen images changed very often, making my laptop look like a big, huge box.
+        My first laptop was made of a box of candies, the keyboard and screen of which was painted with markers, and the screen images changed very often, making my laptop look like a big, huge box.
       </div>
     </div>
 
