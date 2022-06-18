78bba2ad14dfd6837c3a4545f41cf1293b6fec6a
mariamgri22 <mariamgrigoryan99@gmail.com>
2022-06-18 22:08:28 +0400

    initial response commit

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ButtonMailto from '../ButtonMailito'
+import './style.css'
 
 export default function Contact() {
   return (
-    <div>
+    <div className='contact'>
      <ButtonMailto label="Write me an E-Mail" mailto="mailto:mariamgrigoryan99@gmail.com" />
      <p> Or you can call</p>
      <p>+374-94771999</p>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,75 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './style.css'
 
-export default function Header() {
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./style.css";
+
+
+function Header() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
     <>
-    <div className='navbar'>
-    <nav className='container'>
-    <Link  to='/' className='link'>Home</Link>
-    <Link  to='/projects' className='link'>Projects</Link>
-    <Link  to='/about' className='link'>About</Link>
-    <Link  to='/contact' className='link'>Contact</Link>
-    </nav>
-    </div>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            React
+            
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/projects"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
+
+export default Header;
